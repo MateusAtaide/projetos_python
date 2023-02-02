532a366ab27b08957be20568680bb3c5d2da0736
MateusAtaide <55498534+MateusAtaide@users.noreply.github.com>
2023-02-02 13:10:27 -0300

    Update main_calculadora.py

--- main_calculadora.py
+++ main_calculadora.py
@@ -32,5 +32,141 @@ layout = [[sg.Menu(menu_layout)],
            sg.Button('0', font=('Consolas', 20),key='-ZERO-'),
            sg.Button('=', font=('Consolas', 20),key='-IGUAL-')]]
 
+class App():
+    def __init__(self):
+        self.window = sg.Window('PyCalculator', layout=layout, margins=(0,0), resizable=True, return_keyboard=False)
+        self.result = 0
+        self.oper = ''
+        self.window.read(timeout=1)
+    
+    for i in range(1,5):
+        for button in layout[i]:
+            button.expand(expand_x=True, expand_y=True)
+   
+    def about(self):
+        sg.popup('About', 'Just an example', 'contact me')
+    def resulter(self):
+        if self.oper == '+':
+            return float(self.result) + float(self.values['-BOX-'])
+        if self.oper == '-':
+            return float(self.result) - float(self.values['-BOX-'])
+        if self.oper == '*':
+            return float(self.result) * float(self.values['-BOX-'])
+        if self.oper == '/':
+            return float(self.result) / float(self.values['-BOX-'])
+    def start(self):
+        while True:
+            event, self.values = self.window.read()
 
+            if event in (None, 'Exit', sg.WIN_CLOSED):
+                break
 
+            if event in ('About'):
+                self.about()
+
+            if event in ('-UM-'):
+                if self.values['-BOX-'] == '0':
+                    self.window['-BOX-'].update(value='1')
+                else:
+                    self.window['-BOX-'].update(value=self.values['-BOX'] + '1')
+            
+            if event in ('-DOIS-'):
+                if self.values['-BOX-'] == '0':
+                    self.window['-BOX-'].update(value='2')
+                else:
+                    self.window['-BOX-'].update(value=self.values['-BOX'] + '2')
+            
+            if event in ('-TRES-'):
+                if self.values['-BOX-'] == '0':
+                    self.window['-BOX-'].update(value='3')
+                else:
+                    self.window['-BOX-'].update(value=self.values['-BOX'] + '3')
+            
+            if event in ('-QUATRO-'):
+                if self.values['-BOX-'] == '0':
+                    self.window['-BOX-'].update(value='4')
+                else:
+                    self.window['-BOX-'].update(value=self.values['-BOX'] + '4')
+            
+            if event in ('-CINCO-'):
+                if self.values['-BOX-'] == '0':
+                    self.window['-BOX-'].update(value='5')
+                else:
+                    self.window['-BOX-'].update(value=self.values['-BOX'] + '5')
+            
+            if event in ('-SEIS-'):
+                if self.values['-BOX-'] == '0':
+                    self.window['-BOX-'].update(value='6')
+                else:
+                    self.window['-BOX-'].update(value=self.values['-BOX'] + '6')
+
+            if event in ('-SETE-'):
+                if self.values['-BOX-'] == '0':
+                    self.window['-BOX-'].update(value='7')
+                else:
+                    self.window['-BOX-'].update(value=self.values['-BOX'] + '7')
+
+            if event in ('-OITO-'):
+                if self.values['-BOX-'] == '0':
+                    self.window['-BOX-'].update(value='8')
+                else:
+                    self.window['-BOX-'].update(value=self.values['-BOX'] + '8')
+
+            if event in ('-NOVE-'):
+                if self.values['-BOX-'] == '0':
+                    self.window['-BOX-'].update(value='9')
+                else:
+                    self.window['-BOX-'].update(value=self.values['-BOX'] + '9')
+
+            if event in ('-ZERO-'):
+                if self.values['-BOX-'] == '0':
+                    self.window['-BOX-'].update(value='0')
+                else:
+                    self.window['-BOX-'].update(value=self.values['-BOX'] + '0')
+
+            if event in ('-CLEAR-'):
+                self.result = 0
+                self.window['-BOX-'].update(value = self.result)
+
+            if event in ('-BACKARROW'):
+                self.window['-BOX-'].update(value=self.values['-BOX-'][:-1])
+            
+            if event in ('-MAIS-'):
+                if self.oper != '':
+                    self.result = self.resulter()
+                else:
+                    self.oper = '+'
+                    self.result = self.values['-BOX-']
+                self.window['-BOX-'].update(value='')
+            
+            if event in ('-MENOS-'):
+                if self.oper != '':
+                    self.result = self.resulter()
+                else:
+                    self.oper = '-'
+                    self.result = self.values['-BOX-']
+                self.window['-BOX-'].update(value='')
+
+            if event in ('-VEZES-'):
+                if self.oper != '':
+                    self.result = self.resulter()
+                else:
+                    self.oper = '*'
+                    self.result = self.values['-BOX-']
+                self.window['-BOX-'].update(value='')
+
+            if event in ('-DIVISAO-'):
+                if self.oper != '':
+                    self.result = self.resulter()
+                else:
+                    self.oper = '/'
+                    self.result = self.values['-BOX-']
+                self.window['-BOX-'].update(value='')
+
+            if event in ('-IGUAL-'):
+                self.result = self.resulter()
+                self.window['-BOX-'].update(value=self.result)
+                self.result = 0
+                self.oper = ''
+
+App().start()
