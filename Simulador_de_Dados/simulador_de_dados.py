9c16080c6b7980c80c699bb1cd930f1c09b72dd9
mateu <mateus_ataide@hotmail.com>
2023-03-30 22:05:59 -0300

    parcialmente finalizados

--- Simulador_de_Dados/simulador_de_dados.py
+++ Simulador_de_Dados/simulador_de_dados.py
@@ -1,18 +1,22 @@
 import random
-import PySimpleGUI as sg
+import PySimpleGUI as sg #importação das bibliotecas
 
 layout = [
-    [sg.Text('Deseja lançar o dado?')],
-    [sg.Button('SIM'), sg.Button('NÃO')],
+    [sg.Text('Deseja lançar o dado?')],#adiciona um texto a 1ª linha
+    [sg.Button('SIM'), sg.Button('NÃO')], #adiciona dois botões na 2ª linha
     [sg.Text('Aguardando...', key='resultado')],
+    #adiciona uma mensagem na 3ª linha e uma chave para atualização dos valores da mensagem
 ]
 
 window = sg.Window('Simulador de Dado', layout=layout)
+#Cria o programa, e informa o nome na barra de título e qual variável que vai ter o layout descrito
 
-while True:
-    event, values = window.Read()
+while True: #laço de execução do programa
+    event, values = window.Read() #leitura do programa
     if event == sg.WINDOW_CLOSED or event == 'NÃO':
+        #se for fechado ou selecionado o botão "NÃO", o loop vai quebrar
         break
 
-    elif event == 'SIM':
+    elif event == 'SIM': #se for selecionado o botão "SIM" executará os comandos da próxima linha
         window['resultado'].update('Número: ' + str(random.randint(1, 6)))
+        #A mensagem chaveada na 3ª linha é atualizada com um númeo randômico entre 1 e 6
--- login/login.py
+++ login/login.py
@@ -1,36 +1,52 @@
-import PySimpleGUI as sg
+import PySimpleGUI as sg #importação da biblioteca
 
-layout = [
-    [sg.Text('Usuário')],
+layout = [ #inicia-se o layout da janela
+    [sg.Text('Usuário')], #Na 1ª linha é colocado um texto para identificar o campo "usuário"
     [sg.Input(key='usuario', do_not_clear=False)],
-    [sg.Text('Senha')],
+    #Na 2ª linha é colocado um campo para entrar com o nome de usuário
+    #É chaveado e programado com o argumento de limpeza automática do campo
+    [sg.Text('Senha')], #Na 3ª linha é colocado um texto para identificar o campo "senha"
     [sg.Input(key='senha', password_char='*', do_not_clear=False)],
-    [sg.Button('Cadastrar')],
-    [sg.Button('Login')],
+    #Na 4ª linha é colocado um campo para entrar com a senha
+    #É chaveado, programado com um argumento para substituir os caracteres por "*" e
+    #programado com o argumento de limpeza automática do campo
+    [sg.Button('Cadastrar')], #Na 5ª linha é criado o botão de cadastrar
+    [sg.Button('Login')], #Na 6ª linha é criado o botão de login
     [sg.Text('', key='mensagem')],
-]
+    #Na 7ª linha é criado um campo de mensagem ainda vazio, mas que será atualizado pelo seu chaveamento
+] #Terminado o layout da janela
 
 window = sg.Window('Login', layout=layout)
+#Cria o programa, e informa o nome na barra de título e qual variável que vai ter o layout descrito
 
-while True:
-    event, values = window.read()
-    if event == sg.WINDOW_CLOSED:
+while True: #laço de execução do programa
+    event, values = window.read() #leitura do programa
+    if event == sg.WINDOW_CLOSED: #se for fechado o loop vai quebrar
         break
     elif event == 'Cadastrar':
-        try:
-            if (len(values['usuario'].strip()) == 6) and (len(values['senha'].strip()) == 6):
-                usuario_criado = values['usuario'].lower()
-                senha_criada = values['senha']
-                window['mensagem'].update('Usuário ,"' + usuario_criado + '" criado com sucesso!')
-            else:
-                window['mensagem'].update('Preenchimento Inválido!')
-        except:
-            window['mensagem'].update('Preenchimento Inválido2!')
-    elif event == 'Login':
+        #se for selecionado o botão "Cadsatrar", serão executados o seguintes comandos
+        if (len(values['usuario'].strip()) == 6) and (len(values['senha'].strip()) == 6):
+            #neste comando são testados se o tamanho dos valores dos campos são iguais a 6 caracteres
+            usuario_criado = values['usuario'].lower()
+            #uma variável recebe os valores do campo "usuário" em caixa baixa
+            senha_criada = values['senha']
+            #uma variável recebe os valores do campo "senha"
+            window['mensagem'].update('Usuário ,"' + usuario_criado + '" criado com sucesso!')
+            #no campo "mensagem" será exibido o nome do usuário criado, dentro de uma mensagem de sucesso
+        else:
+            window['mensagem'].update('Preenchimento Inválido!')
+            #caso as condições do laço if não forem cumpridas,
+            # será mostrada uma mensagem de erro para repreenchimento dos campos
+    elif event == 'Login': #ao selecionar o botão "Login", os comando abaixo serão executados
         usuario = values['usuario']
+        #uma variável receberá os valores da variavel que continha os valores de "usuário"
         senha = values['senha']
+        # uma variável receberá os valores da variavel que continha os valores de "senha"
         if senha == senha_criada and usuario == usuario_criado:
+            #são comparadas as variáveis do bloco de cadastro com as do bloco de login
             window['mensagem'].update('Login realizado com sucesso!')
+            #se a comparação for positiva, uma mensagem de sucesso será atualizada no campo "mensagem"
         else:
             window['mensagem'].update('Usuario ou senha incorretos!')
+            # se a comparação for negativa, uma mensagem de erro será atualizada no campo "mensagem"
 
