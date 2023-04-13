15f1b5c9816aa3946a533aa872010d5e26b610c1
mateu <mateus_ataide@hotmail.com>
2023-04-13 11:42:42 -0300

    comentarios atualizados

--- Gerenciador_de_Tarefas/Gerenciador_de_Tarefas.py
+++ Gerenciador_de_Tarefas/Gerenciador_de_Tarefas.py
@@ -1,24 +1,30 @@
-import PySimpleGUI as sg
+import PySimpleGUI as sg #importação das bibliotecas necessárias
 
-def criar_janela_inicial():
-    sg.theme('Dark')
+def criar_janela_inicial(): #função de criação do layout da janela
+    sg.theme('Dark')#tema da janela
     linha = [
         [sg.Checkbox(''), sg.Input('')]
+        #criação de um layout de linha com uma entrada de chqcagem e ma caixa de texto
     ]
-    layout = [
+    layout = [ #criação do layout padrão
         [sg.Frame('Tarefas', layout=linha, key='container')],
+        #na linha 1 é criado um quadro que vai ter imbutido os layouts de linha, e chaveado como "container"
         [sg.Button('Nova Tarefa'), sg.Button('Resetar')]
+        #na linha 2 são criados dois botões
     ]
-    return sg.Window('Todo List', layout=layout, finalize=True)
+    return sg.Window('Todo List', layout=layout, finalize=True) #retorno da função será a própria janela
 
-janela = criar_janela_inicial()
+janela = criar_janela_inicial() #atribuição de função à variável
 
-while True:
-    event, values = janela.read()
-    if event == sg.WINDOW_CLOSED:
+while True: #laço de leitura da janela
+    event, values = janela.read() #leitura de eventos e valores da janela
+    if event == sg.WINDOW_CLOSED: #laço para o caso do botão fechar for clicado, o laço quebra
         break
-    elif event == 'Nova Tarefa':
+    elif event == 'Nova Tarefa': #criação do evento para o botão "Nova Tarefa"
         janela.extend_layout(janela['container'], [[sg.Checkbox(''), sg.Input('')]])
-    elif event == 'Resetar':
-        janela.close()
-        janela = criar_janela_inicial()
+        #cria novas linhas dentro da variável janela
+    elif event == 'Resetar': #criação do evento para o botão "Resetar"
+        janela.close() #limpa e fecha a janela
+        janela = criar_janela_inicial() #cria uma nova janela limpa
+
+    #fim do código
--- Tradutor_Pt-En/tradutor_Pt-En.py
+++ Tradutor_Pt-En/tradutor_Pt-En.py
@@ -1,19 +1,27 @@
 from translate import Translator
 import translate
-import PySimpleGUI as sg
+import PySimpleGUI as sg #importação das bibliotecas necessárias
 
-layout = [
+layout = [ #criação do layout
     [sg.Text('Frase/Termo:'), sg.Input('', key='entrada'), sg.Button('Traduzir')],
+    #na 1 linha cria-se um texto padrão, uma caixa de entrada de texto chaveada como "entrada", e um botão
     [sg.Text('', key='traducao')]
+    #na linha 2 cria-se um texto padrão ainda em branco, chaveado como "tradução"
 ]
 
 window = sg.Window('Tradutor Pt-Br -> En', layout=layout)
+#criação da janela com titulo e layout
 t = Translator(from_lang='pt-br', to_lang='english')
+#criação da variável de tradução, com os parâmetros de tradução
 
-while True:
-    event, values = window.read()
-    if event == sg.WINDOW_CLOSED:
+while True: #laço de leitura da janela
+    event, values = window.read() #leitura dos eventos e valores da janela
+    if event == sg.WINDOW_CLOSED: #laço para o caso do botão fechar for clicado, o laço quebra
         break
-    if event == 'Traduzir':
+    if event == 'Traduzir': #criação do evento ao clicar no botão "Traduzir"
         traducao = t.translate(values['entrada'])
+        #a variável "t" irá traduzir o texto e inserir em outra variável
         window['traducao'].update(f'Tradução: {traducao}')
+        #na janela será atualizada a mensagem na chave "traducao" para o conteudo traduzido
+
+        #fim do código
