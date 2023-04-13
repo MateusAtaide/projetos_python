630269e4acc241256f4a0151860db2970bdae7bf
mateu <mateus_ataide@hotmail.com>
2023-04-13 20:21:08 -0300

    comentarios atualizados

--- Player_de_Musicas/player.py
+++ Player_de_Musicas/player.py
@@ -1,77 +1,87 @@
 import PySimpleGUI as sg
 import os
 from utils.music_utilities import get_files_inside_directory_not_recursive, play_sound, is_sound_playing, pause_sounds, stop_sounds, unpause
+#importação das bibliotecas necesárias, inclusive da pasta utils
+sg.theme('Reddit') #escolha do tema
 
-sg.theme('Reddit')
-song_title_column = [
-    [sg.Text(text='Press play..', justification='center', background_color='black',
+#definição dos layouts da janela
+song_title_column = [ #texto de inicio da janela principal com seu chaveamento
+    [sg.Text(text='Pressione Play...', justification='center', background_color='black',
              text_color='white', size=(200, 0), font='Tahoma', key='song_name')]
 ]
 
-player_info = [
+player_info = [ #texto sobrea a capa e status da lista de reprodução e seu chaveamento
     [sg.Text('PySimpleGUI Player',
              background_color='black', text_color='white',  font=('Tahoma', 10), key='player_info')]
 ]
 
-currently_playing = [
+currently_playing = [ #texto com o titulo da musica sendo reproduzida e seu chaveamento
     [sg.Text(background_color='black', size=(200, 0), text_color='white',
              font=('Tahoma', 10), key='currently_playing')]
 ]
+#bloco de imagens que serão utilizadas para botões e e capa
+imagem_voltar = 'Images\\back.png'
+imagem_proxima = 'Images\\next.png'
+imagem_play = 'Images\\play.png'
+imagem_pause = 'Images\\pause.png'
+imagem_album = 'Images\\songs.png'
 
-GO_BACK_IMAGE_PATH = 'Images\\back.png'
-GO_FORWARD_IMAGE_PATH = 'Images\\next.png'
-PLAY_SONG_IMAGE_PATH = 'Images\\play_button.png'
-PAUSE_SONG_IMAGE_PATH = 'Images\\pause.png'
-ALBUM_COVER_IMAGE_PATH = 'Images\\pylot.png'
-
-main = [
-    [sg.Canvas(background_color='black', size=(480, 20), pad=None)],
+main = [ #criasção do layout da janela
+    [sg.Canvas(background_color='black', size=(480, 20), pad=None)],#cria um fundo preto
     [sg.Column(layout=player_info, justification='c',
                element_justification='c', background_color='black')],
-    [
+    [ #insere o texto de inicio já definido acima
         sg.Canvas(background_color='black', size=(40, 350), pad=None),
-        sg.Image(filename=ALBUM_COVER_IMAGE_PATH,
+        sg.Image(filename=imagem_album,
                  size=(350, 350), pad=None),
         sg.Canvas(background_color='black', size=(40, 350), pad=None)
-    ],
+    ],#insere a imagem do album ao centro
     [sg.Canvas(background_color='black', size=(480, 10), pad=None)],
     [sg.Column(song_title_column, background_color='black',
                justification='c', element_justification='c')],
-    [sg.Text('_'*80, background_color='black', text_color='white')],
-    [
-        sg.Canvas(background_color='black', size=(99, 200), pad=(0, 0)),
-        sg.Image(pad=(10, 0), filename=GO_BACK_IMAGE_PATH, enable_events=True,
-                 size=(35, 44), key='previous', background_color='black'),
-        sg.Image(filename=PLAY_SONG_IMAGE_PATH,
+    #alinha o texto da titulo da musica ainda sem titulo
+    [sg.Text('_'*80, background_color='black', text_color='white')],#cria uma linha separando os botões
+    [#bloco de layout dos botões
+        sg.Canvas(background_color='black', size=(64, 150), pad=(0, 0)),
+        sg.Image(pad=(10, 0), filename=imagem_voltar, enable_events=True,
+                 size=(64, 64), key='previous', background_color='black'),
+        sg.Image(filename=imagem_play,
                  size=(64, 64), pad=(10, 0), enable_events=True, key='play', background_color='black'),
-        sg.Image(filename=PAUSE_SONG_IMAGE_PATH,
-                 size=(58, 58), pad=(10, 0), enable_events=True, key='pause', background_color='black'),
-        sg.Image(filename=GO_FORWARD_IMAGE_PATH, enable_events=True,
-                 size=(35, 44), pad=(10, 0), key='next', background_color='black'),
+        sg.Image(filename=imagem_pause,
+                 size=(64, 64), pad=(10, 0), enable_events=True, key='pause', background_color='black'),
+        sg.Image(filename=imagem_proxima, enable_events=True,
+                 size=(64, 64), pad=(10, 0), key='next', background_color='black'),
     ],
     [sg.Column(layout=currently_playing, justification='c',
                element_justification='c', background_color='black', pad=None)]
+    #alinha o titulo da musica depois de carregada a pasta
 ]
-window = sg.Window('Player', layout=main, size=(
+
+#cria a janela com titulo e layout
+window = sg.Window('Player de Músicas', layout=main, size=(
     480, 730), background_color='black', finalize=True, grab_anywhere=True, resizable=False)
 
 directory = sg.popup_get_folder('Selecione a pasta de músicas!')
+#cria um popup no inicio da aplicação para capturar o diretório dos arquivos
 
+#utiliza as funções do arquivo utils para capturar as informações do diretório
 songs_in_directory = get_files_inside_directory_not_recursive(directory)
 song_count = len(songs_in_directory)
 current_song_index = 0
 
 def update_song_display():
+    #a apartir das informações contidas no diretório dos arquivos atualiza o nome da música exibido
     window['song_name'].update(os.path.basename(
         songs_in_directory[current_song_index]))
     window['currently_playing'].update(
         f'Playing: {os.path.basename(songs_in_directory[current_song_index])}')
 
-while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED:
+while True: #laço para leitura de eventos e valores
+    event, values = window.read()#leitura de eventos e valores
+    if event == sg.WIN_CLOSED:#ao apertar o botão "fechar" o laço de leitura quebra
         break
-    elif event == 'play':
+    elif event == 'play': #laço para o evento play, verifica se existe uma musica reproduzindo,
+        #se não reproduz o que houver na lista
         if is_sound_playing():
             pass
         if is_sound_playing() == False:
@@ -85,22 +95,26 @@ while True:
             unpause()
         pass
 
-    elif event == 'next':
+    elif event == 'next': #laço do evento "next"
+        #avança a contagem da lista de músicas para a que está reproduzindo e passa para próxima música
         if current_song_index + 1 < song_count:
             stop_sounds()
             current_song_index += 1
             play_sound(songs_in_directory[current_song_index])
             update_song_display()
 
-        else:
+        else: #se o contador estiver chagado ao limite do indice da lista, gera a mensagem abaixo
             window['player_info'].update("Chegou ao fim da lista!")
             pass
 
-    elif event == 'previous':
+    elif event == 'previous': #laço do evento "previous"
+        #reduz a contagem da lista de músicas para a que está reproduzindo e retorna para a música anterior
         if current_song_index + 1 <= song_count and current_song_index > 0:
             stop_sounds()
             current_song_index -= 1
             play_sound(songs_in_directory[current_song_index])
             update_song_display()
-        else:
+        else: #se estiver na primeira música, o contador chegará no limite a lista e irá gerar a mensagem abaixo
             window['player_info'].update("Chegou ao fim da lista!")
+
+     #fim do código
