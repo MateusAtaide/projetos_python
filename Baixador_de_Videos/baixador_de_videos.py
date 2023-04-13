ff840d91024b2476321c6705964eed61edad6f7e
mateu <mateus_ataide@hotmail.com>
2023-04-13 17:39:09 -0300

    comentarios atualizados

--- Baixador_de_Videos/baixador_de_videos.py
+++ Baixador_de_Videos/baixador_de_videos.py
@@ -1,24 +1,33 @@
 import pytube
 from pytube.exceptions import RegexMatchError
-import PySimpleGUI as sg
+import PySimpleGUI as sg #importação das bibliotecas necessárias
 
-layout = [
+layout = [ #criação do layout
     [sg.Text('Insira o link do YouTube:')], [sg.Input(key='link')],
-    [sg.Button('Baixar')], [sg.Button('Cancelar')],
+    #na linha 1  e 2 criam-se: um texto padrão uma caixa de texto e   para inserção de dados e chaveada com "link"
+    [sg.Button('Baixar'), sg.Button('Cancelar')],
+    #na linha 3 criam-se dois botões
     [sg.Text('', key='mensagem')]
+    #na linha 4 cria-se uma caixa de texto vazia e com chaveamento de "mensagem"
 ]
 window = sg.Window('Baixador de Videos', layout=layout)
+#criação da janela com titulo e layout
 
-while True:
-    event, values = window.read()
+while True: #laço de leitura da janela
+    event, values = window.read() #leitura dos eventos e valores da janela
     if event == sg.WINDOW_CLOSED or event == 'Cancelar':
+        #laço para o caso do botão fechar ou "Cancelar" for clicado, o laço quebra
         break
     try:
         event == 'Baixar' and values['link'] != ''
-        link = values['link']
-        yt = pytube.YouTube(link)
+        # teste para o evento do botão "Baixar", se for clicado e se a caixa de texto "link" não estiver vazia
+        link = values['link'] #guarda os valores de link em uma variável
+        yt = pytube.YouTube(link) #captura o link através da função da biblioteca Pytube e insere em uma variável
         yt.streams.get_highest_resolution().download()
-        window['mensagem'].update('Download Concluído!')
+        #utiliza mais uma função da biblioteca, agora para fazer o dowload do link na maior qualidade disponível
+        window['mensagem'].update('Download Concluído!') #atualiza o campo "mensagem" com uma mensagem de sucesso
     except RegexMatchError:
-        window['mensagem'].update('Link inválido!')
+        window['mensagem'].update('Link inválido!') #tratamento de erro de URL inávilda
+
+        #fim do código
 
--- Cauculadora/calculadora.py
+++ Cauculadora/calculadora.py
@@ -1,142 +1,156 @@
-import PySimpleGUI as sg
+import PySimpleGUI as sg #mportação de biblioteca
 
-sg.theme('Black')
+sg.theme('Black') #definição do tema a ser usado na janela
 
-jan_alt = 30
-jan_com = 50
+jan_alt = 30 #definição de altura da janela
+jan_com = 50 #definição do comprimento da janela
 
-menu_layout = [['File', ['Save', 'Exit']],
+menu_layout = [['File', ['Save', 'Exit']], #estrutura de menu com botões padrão
                ['Tools', ['Waiting']],
                ['Help', ['About']]]
 
-layout = [[sg.Menu(menu_layout)],
+layout = [[sg.Menu(menu_layout)], # na linha 1 utiliza-se o menu já definido
           [sg.Input('0', size=(int(jan_alt / 2), 1), font=('Consolas', 20), key='-BOX-'),
            sg.Button('<-', font=('Consolas', 20), key='-BACKARROW-'),
-           sg.Button('<-', font=('Consolas', 20), key='-CLEAR-')],
+           sg.Button('AC', font=('Consolas', 20), key='-CLEAR-')],
+            #na linha 2 cria-se uma caixa de texto e botões para as funções de apagar e limpar
+            #com seus respectivos chaveamentos e parametros
 
           [sg.Button('7', font=('Consolas', 20), key='-SETE-'),
            sg.Button('8', font=('Consolas', 20), key='-OITO-'),
            sg.Button('9', font=('Consolas', 20), key='-NOVE-'),
            sg.Button('+', font=('Consolas', 20), key='-MAIS-'),
            sg.Button('*', font=('Consolas', 20), key='-VEZES-')],
+            #na linha 3 criam-se botões para as funções e numeros
+            #com seus respectivos chaveamentos e parametros
 
           [sg.Button('4', font=('Consolas', 20), key='-QUATRO-'),
            sg.Button('5', font=('Consolas', 20), key='-CINCO-'),
            sg.Button('6', font=('Consolas', 20), key='-SEIS-'),
            sg.Button('-', font=('Consolas', 20), key='-MENOS-'),
            sg.Button('/', font=('Consolas', 20), key='-DIVISAO-')],
+          # na linha 4 criam-se botões para as funções e numeros
+          # com seus respectivos chaveamentos e parametros
 
           [sg.Button('1', font=('Consolas', 20), key='-UM-'),
            sg.Button('2', font=('Consolas', 20), key='-DOIS-'),
            sg.Button('3', font=('Consolas', 20), key='-TRES-'),
            sg.Button('0', font=('Consolas', 20), key='-ZERO-'),
            sg.Button('=', font=('Consolas', 20), key='-IGUAL-')]]
-
-
-class App():
-    def __init__(self):
-        self.window = sg.Window('PyCalculator', layout=layout, margins=(0, 0), resizable=True,
+            # na linha 5 criam-se botões para as funções e numeros
+            # com seus respectivos chaveamentos e parametros
+class App(): #criação da classe principal
+    def __init__(self): #função de inicialização
+        self.window = sg.Window('Calculadora PySimpleGUI', layout=layout, margins=(0, 0), resizable=True,
                                 return_keyboard_events=False)
+        #Ccriação da janela com titulo, layout, margens, tamanho ajustável de janela e sem entrada pelo teclado
         self.result = 0
+        #inicia com valor zero na tela
         self.oper = ''
+        #inicia sem operção
         self.window.read(timeout=1)
+        #leitura da janela com tempo de 1 milisegundo para leitura
 
-        for i in range(1, 5):
+        for i in range(1, 5): #parametrização de expanção dinamica dos botões em conjunto com a janela
             for button in layout[i]:
                 button.expand(expand_x=True, expand_y=True)
     finalize = True
+    #finaliza a parametrizaçõa da janela
+    def about(self): #função pra mostrar um popup ao clicar em "About"
+        sg.popup('Sobre', 'Programa Calculadora Versão 0.0', 'Contato:00000000')
 
-    def about(self):
-        sg.popup('About', 'Just an example', 'contact me')
-
-    def resulter(self):
+    def resulter(self): #bloco de funções para chamada das operções da cauculadora
         if self.oper == '+':
             return float(self.result) + float(self.values['-BOX-'])
+        #ao apertar o '+' pega os valores digitados na caixa e soma
         if self.oper == '-':
             return float(self.result) - float(self.values['-BOX-'])
+        # ao apertar o '-' pega os valores digitados na caixa e subtrai
         if self.oper == '*':
             return float(self.result) * float(self.values['-BOX-'])
+        # ao apertar o '*' pega os valores digitados na caixa e multiplica
         if self.oper == '/':
             return float(self.result) / float(self.values['-BOX-'])
-
-    def start(self):
-        while True:
-            event, self.values = self.window.read()
-
+        # ao apertar o '/' pega os valores digitados na caixa e divide
+    def start(self): #função para iniciar a leitura de eventos e valores
+        while True: #laço para leitura de eventos e valores
+            event, self.values = self.window.read()#leitura de eventos e valores
             if event in (None, 'Exit', sg.WIN_CLOSED):
+                #ao apertar o botão "fechar" ou "exit" o laço de leitura quebra
                 break
-
-            if event in ('About'):
+            if event in ('About'): #ao clicar em "about" é mostrado o popup já configurado
                 self.about()
-
             if event in ('-UM-'):
                 if self.values['-BOX-'] == '0':
                     self.window['-BOX-'].update(value='1')
                 else:
                     self.window['-BOX-'].update(value=self.values['-BOX-'] + '1')
+            #bloco de códigos para o botão 1, ao apertá-lo o valor na tela muda de 0 para 1,
+            # ou se ja houver um caractere 1, isere mais 1 -> 11...1
 
-            if event in ('-DOIS-'):
+            if event in ('-DOIS-'): #mesmo procedimento do bloco do botão 1
                 if self.values['-BOX-'] == '0':
                     self.window['-BOX-'].update(value='2')
                 else:
                     self.window['-BOX-'].update(value=self.values['-BOX-'] + '2')
 
-            if event in ('-TRES-'):
+            if event in ('-TRES-'):#mesmo procedimento do bloco do botão 1
                 if self.values['-BOX-'] == '0':
                     self.window['-BOX-'].update(value='3')
                 else:
                     self.window['-BOX-'].update(value=self.values['-BOX-'] + '3')
 
-            if event in ('-QUATRO-'):
+            if event in ('-QUATRO-'):#mesmo procedimento do bloco do botão 1
                 if self.values['-BOX-'] == '0':
                     self.window['-BOX-'].update(value='4')
                 else:
                     self.window['-BOX-'].update(value=self.values['-BOX-'] + '4')
 
-            if event in ('-CINCO-'):
+            if event in ('-CINCO-'):#mesmo procedimento do bloco do botão 1
                 if self.values['-BOX-'] == '0':
                     self.window['-BOX-'].update(value='5')
                 else:
                     self.window['-BOX-'].update(value=self.values['-BOX-'] + '5')
 
-            if event in ('-SEIS-'):
+            if event in ('-SEIS-'):#mesmo procedimento do bloco do botão 1
                 if self.values['-BOX-'] == '0':
                     self.window['-BOX-'].update(value='6')
                 else:
                     self.window['-BOX-'].update(value=self.values['-BOX-'] + '6')
 
-            if event in ('-SETE-'):
+            if event in ('-SETE-'):#mesmo procedimento do bloco do botão 1
                 if self.values['-BOX-'] == '0':
                     self.window['-BOX-'].update(value='7')
                 else:
                     self.window['-BOX-'].update(value=self.values['-BOX-'] + '7')
 
-            if event in ('-OITO-'):
+            if event in ('-OITO-'):#mesmo procedimento do bloco do botão 1
                 if self.values['-BOX-'] == '0':
                     self.window['-BOX-'].update(value='8')
                 else:
                     self.window['-BOX-'].update(value=self.values['-BOX-'] + '8')
 
-            if event in ('-NOVE-'):
+            if event in ('-NOVE-'):#mesmo procedimento do bloco do botão 1
                 if self.values['-BOX-'] == '0':
                     self.window['-BOX-'].update(value='9')
                 else:
                     self.window['-BOX-'].update(value=self.values['-BOX-'] + '9')
 
-            if event in ('-ZERO-'):
+            if event in ('-ZERO-'):#mesmo procedimento do bloco do botão 1
                 if self.values['-BOX-'] == '0':
                     self.window['-BOX-'].update(value='0')
                 else:
                     self.window['-BOX-'].update(value=self.values['-BOX-'] + '0')
 
-            if event in ('-CLEAR-'):
+            if event in ('-CLEAR-'): #limpa os valores do input e atualiza para zero
                 self.result = 0
                 self.window['-BOX-'].update(value=self.result)
 
-            if event in ('-BACKARROW-'):
+            if event in ('-BACKARROW-'): #apaga as ultimas posições da string digitada através de slice
                 self.window['-BOX-'].update(value=self.values['-BOX-'][:-1])
 
             if event in ('-MAIS-'):
+                #bloco chama a função da operação digitada e atualiza na tela e nos paramentros das variáveis
                 if self.oper != '':
                     self.result = self.resulter()
                 else:
@@ -144,7 +158,7 @@ class App():
                     self.result = self.values['-BOX-']
                 self.window['-BOX-'].update(value='')
 
-            if event in ('-MENOS-'):
+            if event in ('-MENOS-'): #mesmo comportamento do bloco de adição, para subtração
                 if self.oper != '':
                     self.result = self.resulter()
                 else:
@@ -152,7 +166,7 @@ class App():
                     self.result = self.values['-BOX-']
                 self.window['-BOX-'].update(value='')
 
-            if event in ('-VEZES-'):
+            if event in ('-VEZES-'): #mesmo comportamento do bloco de adição, para multiplicação
                 if self.oper != '':
                     self.result = self.resulter()
                 else:
@@ -160,7 +174,7 @@ class App():
                     self.result = self.values['-BOX-']
                 self.window['-BOX-'].update(value='')
 
-            if event in ('-DIVISAO-'):
+            if event in ('-DIVISAO-'): #mesmo comportamento do bloco de adição, para divisão
                 if self.oper != '':
                     self.result = self.resulter()
                 else:
@@ -168,12 +182,13 @@ class App():
                     self.result = self.values['-BOX-']
                 self.window['-BOX-'].update(value='')
 
-            if event in ('-IGUAL-'):
+            if event in ('-IGUAL-'): #chama as variáveis de resultados e atualiza na tela
                 self.result = self.resulter()
                 self.window['-BOX-'].update(value=self.result)
-                self.result = 0
-                self.oper = ''
+                self.result = 0 #zera a varíavel para ser usada novamente
+                self.oper = '' #limpa a variável para ser utilizada novamente
 
+App().start() #iniciador da classe
 
-App().start()
+#fim do código
 
